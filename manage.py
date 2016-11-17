2cd17f955ea21c2373387002597425b432a367a6
B.Sai Kiriti <kiriti163.iitm@gmail.com>
2016-11-18 00:37:31 +0900

    Testing web editing in Git

--- manage.py
+++ manage.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniproject.settings")
 
     from django.core.management import execute_from_command_line
+    #Random comment testing on git
 
     execute_from_command_line(sys.argv)
